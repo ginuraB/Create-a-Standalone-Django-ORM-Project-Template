3a56b31b13403fde0383b61e92a3b601071802ea
ginuraB <uniginura@gmail.com>
2025-01-21 09:09:05 +0530

    django

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '22dxnyKWiiZHH96PJPIwS8aQ',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
